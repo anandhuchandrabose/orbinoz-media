c00cf7524a9c1ead02e5fd0809fb8e76f0fc2e9e
anandhuchandrabose <anandhuchandrabose2000@gmail.com>
2024-07-31 12:59:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// src/App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './LandingPage';
+// import WhatAreWe from './components/WhatAreWe';
+import reportWebVitals from './reportWebVitals';
+// import './index.css';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <LandingPage />
+     
+    // </div>
+    <Router>
+      <Routes>
+          <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
